80a03a85a6bf8092ded612a76538e6b1630350eb
tirlochanarora16 <tirlochan.arora16@gmail.com>
2022-03-01 11:40:22 +0530

    made the phone number clickable, and added router for contact us button to redirect to location page

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,13 +1,25 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import style from "./Contact.module.scss";
 
 const Contact = () => {
+  const router = useRouter();
+
+  const contactUsHandler = () => router.push("/location");
+
   return (
     <div className={style.contact}>
       <div className={style["contact__number"]}>
-        <div className={style["contact__number-1"]}>06.83.05.97.01</div>
-        <div className={style["contact__number-2"]}>01.45.31.11.20</div>
+        <div className={style["contact__number-1"]}>
+          <Link href="tel: +330683059701">06.83.05.97.01</Link>
+        </div>
+        <div className={style["contact__number-2"]}>
+          <Link href="tel: +330145311120">01.45.31.11.20</Link>
+        </div>
       </div>
-      <div className={style["contact__button"]}>Nous contacter</div>
+      <div className={style["contact__button"]} onClick={contactUsHandler}>
+        Nous contacter
+      </div>
     </div>
   );
 };
